2d064567be313565d967c32d6029da0b5b4518b4
tabishhameed <tabishhameed34@gmail.com>
2020-12-19 03:31:09 +0800

    writing a file using writing method

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ file_content = my_file.read()
 
 my_file.close()
 print(file_content)
+
+
+user_name = input('enter your name')
+my_file_writing = open('data.txt', 'w')
+my_file_writing.write(user_name)
+my_file_writing.close()
+
+print(my_file_writing)
