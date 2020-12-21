017cfe987fb4d101ddb2cd6651fcb0b0fa909af5
tabishhameed <tabishhameed34@gmail.com>
2020-12-22 03:58:42 +0800

    create dictionary and store in txt using dump fuction

--- json files.py
+++ json files.py
@@ -1,7 +1,16 @@
 import json
-
 file = open('friends_json.txt', 'r')
 file_content = json.load(file)
 file.close()
 
 print(file_content['friends'][0])
+
+
+cars = [
+    {
+        'name': 'ford', 'model': 'fiesta'
+    }
+]
+file_1 = open('cars_json.txt', 'w')
+json.dump(cars,file_1)
+file_1.close()
