c138d0a80430fc673b4738a48575c848699b4f0d
tabishhameed <tabishhameed34@gmail.com>
2020-12-29 04:56:11 +0800

    open file with new method (with)

--- csv convert to json.py
+++ csv convert to json.py
@@ -1,15 +1,15 @@
 import json
-file_operate = open('csv_file.txt', 'r')
-file_convert = [line.strip() for line in file_operate.readlines()]
+with open('csv_file.txt', 'r') as file_operate:
+    file_convert = [line.strip() for line in file_operate.readlines()]
 
-file_operate.close()
+
 result = []
 for line in file_convert:
     a = line.split(',')
     dict = {"club": a[0],"country": a[2], "city":a[1]}
     result.append(dict)
 
-file_open = open('json_file.txt', 'w')
-file_open.write(json.dumps(result))
-file_open.close()
+with open('json_file.txt', 'w') as file_open:
+    file_open.write(json.dumps(result))
+
 
